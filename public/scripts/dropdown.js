cacff262074f62014d89b953ab610c5f6d1a2954
1manuelc <manuelwn21@gmail.com>
2025-06-02 18:55:28 -0300

    refactor: separa responsabilidades dos script

---
+++ public/scripts/dropdown.js
@@ -0,0 +1,31 @@
+(function startDropdown() {
+	const dropdownList = document.querySelector('.dropdown-list');
+	const customDropdown = document.querySelector('.custom-dropdown');
+	const dropdownHeader = document.querySelector('.dropdown-header');
+	const lanBarTop = document.querySelector('.lan-bar-top');
+
+	document.addEventListener('click', function (event) {
+		if (
+			dropdownList.style.display === 'block' &&
+			!dropdownHeader.contains(event.target)
+		) {
+			toggleDropdown(dropdownList, lanBarTop, customDropdown);
+		}
+	});
+
+	dropdownHeader.addEventListener('click', () =>
+		toggleDropdown(dropdownList, lanBarTop, customDropdown)
+	);
+})();
+
+function toggleDropdown(dropdownList, lanBarTop, customDropdown) {
+	if (dropdownList.style.display === 'block') {
+		dropdownList.style.display = 'none';
+		lanBarTop.classList.remove('open');
+		customDropdown.classList.remove('open');
+	} else {
+		dropdownList.style.display = 'block';
+		lanBarTop.classList.add('open');
+		customDropdown.classList.add('open');
+	}
+}
---
+++ public/scripts/formulas.js
@@ -0,0 +1,43 @@
+function calculateImc(weight, height) {
+	if (weight <= 0 || height <= 0) {
+		return null;
+	}
+
+	const heightInMeters = height / 100;
+	const imc = weight / heightInMeters ** 2;
+	return imc.toFixed(2);
+}
+
+function getImcClassification(imc) {
+	if (imc < 18.5) return 'result-imc-classification-underweight';
+	if (imc >= 18.5 && imc <= 24.9) return 'result-imc-classification-normal';
+	if (imc >= 25 && imc <= 29.9) return 'result-imc-classification-overweight';
+	if (imc > 30 && imc <= 34.9) return 'result-imc-classification-obesity-i';
+	if (imc > 35 && imc <= 39.9) return 'result-imc-classification-obesity-ii';
+	if (imc > 40) return 'result-imc-classification-obesity-iii';
+	return '';
+}
+
+/* função para calcular a taxa metabólica basal e o nível de calorias necessárias
+de acordo com a prática esportiva*/
+function calculateTmb(weight, age, height, gender) {
+	const tmbRate =
+		gender === 'Masculino'
+			? 10 * weight + 6.25 * height - 5 * age + 5
+			: 10 * weight + 6.25 * height - 5 * age - 161;
+
+	const tmbData = {
+		tmb: tmbRate,
+		sedentary_calories: tmbRate * 1.2,
+		light_exercise_calories: 1.375 * tmbRate,
+		moderate_exercise_calories: 1.55 * tmbRate,
+		active_exercise_calories: 1.725 * tmbRate,
+		super_active_exercise_calories: 1.9 * tmbRate,
+		to_weight_gain_calories: tmbRate + 450,
+		to_weight_loss_calories: tmbRate - 450,
+	};
+
+	return tmbData;
+}
+
+export { calculateImc, getImcClassification, calculateTmb };
